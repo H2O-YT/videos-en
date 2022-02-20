a9bc9ab2be1200fdf952bac7f3161100462d4ce8
H2O-YT <benjamin.ubilla.29@gmail.com>
2022-02-20 23:00:46 +0000

    Updated

--- 2022/limits/epsilon_delta.py
+++ 2022/limits/epsilon_delta.py
@@ -103,66 +103,124 @@ class Thumbnail(EpsilonDeltaScene):
         return (x**3 - 1)/(x - 1) - 4
 
 
-class TeachFunctionsScene(Scene):
-    
-    def setup_function_stuff(self, function, function_strings, x_values, discontinuities):
+class TeachFunctionScene(Scene):
+
+    def setup_function_stuff(self, function, x_vals, steps, x_str, y_str, f_str):
         self.function = function
-        self.function_strings = function_strings
-        self.x_values = x_values
-        self.y_values = [function(x) for x in x_values]
-        self.discontinuities = discontinuities
-    
-    def construct(self):
+        self.x_vals = x_vals
+        self.steps = steps
+        self.x_str = x_str
+        self.y_str = y_str
+        self.f_str = f_str
 
-        self.show_function_mapping()
-
-    def get_group(self, var_string, var_values, color):
-
+    def get_function_steps_group(self, x_val=None):
         result = VGroup()
 
-        for var in var_values:
-            tex = MathTex(var_string, "=", var).set_color_by_tex(var_string, color)
+        steps = self.steps
+
+        if x_val == None:
+            for i in range(len(steps[0])):
+                if isinstance(steps[0][i], function):
+                    steps[0][i] = self.x_str
+            tex = MathTex(self.f_str, "(", self.x_str, ")", "=", *steps[0])
             result.add(tex)
         
-        result.arrange(DOWN)
-        ellipse = Ellipse(width=result.width+2.0, height=result.height+2.0)
-        ellipse.set_color(color)
-        result.add(ellipse)
+        else:
+            for i in range(len(steps)):
+                for j in range(len(steps[i])):
+                    if isinstance(steps[i][j], function):
+                        steps[i][j] = steps[i][j](x_val)
+                        if i == 0:
+                            steps[i][j] = "("+str(x_val)+")"
+                tex = MathTex(self.f_str, "(", x_val, ")", "=", *steps[i])
+                result.add(tex)
+            tex = MathTex(self.f_str, "(", x_val, ")", "=", self.function(x_val))
+            result.add(tex)
+        
+        for part in result:
+            part.set_color_by_tex(self.x_str, YELLOW)
+            part.set_color_by_tex(self.f_str, GREEN)
+            part.to_edge(DOWN)
         
         return result
-    
-    def show_function_mapping(self):
 
-        input_group = self.get_group("x", self.x_values, YELLOW)
-        output_group = self.get_group("y", self.y_values, BLUE)
-        groups = VGroup(input_group, output_group).arrange(RIGHT)
+    def get_y_tex(self, x_val=None):
 
-        arrow = CurvedArrow(
-            *[group.get_top()+0.2*UP for group in groups], angle=-PI/4
-        )
-        function_tex = MathTex(self.function_strings[0])
-        function_tex.set_color(GREEN)
-        function_tex.next_to(arrow, UP)
-        arrow_group = VGroup(arrow, function_tex)
-
-        f_x = MathTex(
-            self.function_strings[0], "(", "x", ")", "=", *self.function_strings[1]
-        )
-        f_x.set_color_by_tex("x", YELLOW)
-        f_x.set_color_by_tex(self.function_strings[0], GREEN)
-        f_x.to_edge(DOWN)
-        all_groups = VGroup(*groups, arrow_group, f_x)
+        if x_val == None:
+            tex = MathTex(self.y_str, "=", self.f_str, "(", self.x_str, ")")
+        else:
+            tex = MathTex(self.y_str, "=", self.f_str, "(", x_val, ")")
         
-        for group in all_groups:
-            self.play(Write(group))
-            self.wait()
+        tex.set_color_by_tex(self.f_str, GREEN)
+        tex.set_color_by_tex(self.x_str, YELLOW)
+        tex.set_color_by_tex(self.y_str, BLUE)
+        
+        return tex
+    
+    def get_input_output_group(self):
+        
+        x_vals = self.x_vals
+        y_vals = [self.function(x) for x in self.x_vals]
+        x_group = VGroup(VGroup(), VGroup())
+        y_group = VGroup(VGroup(), VGroup())
+        arrow_group = VGroup()
+        result = VGroup()
 
+        for x, y in zip(x_vals, y_vals):
+            x_tex = MathTex(self.x_str, "=", x)
+            x_tex.set_color_by_tex(self.x_str, YELLOW)
+            x_group[0].add(x_tex)
+            y_tex = MathTex(self.y_str, "=", y)
+            y_tex.set_color_by_tex(self.y_str, BLUE)
+            y_group[0].add(y_tex)
+        
+        x_group[0].arrange(DOWN)
+        y_group[0].arrange(DOWN)
 
-class FirstFunction(TeachFunctionsScene):
+        colors = [YELLOW, BLUE]
+        tex_groups = VGroup(x_group, y_group)
+        tex_sets = VGroup(MathTex("A"), MathTex("B"))
+        for tex_group, color, tex_set in zip(colors, tex_groups, tex_sets):
+            ellipse = Ellipse(width=tex_group[0].width+1.0, height=tex_group[0].height+1.0)
+            ellipse.set_color(color)
+            ellipse.surround(tex_group)
+            tex_set.next_to(ellipse, UP)
+            tex_set.set_color(RED)
+            tex_group[1].add(ellipse, tex_set)
+        
+        arrow = CurvedArrow(
+            x_group[1][1].get_top()+0.2*UR,
+            y_group[1][1].get_top()+0.2*UL,
+            angle=-PI/4
+        )
+
+        result.add(x_group, y_group)
+        result.arrange(RIGHT, buff=0.5)
+
+        arrow_tex = MathTex(self.f_str).set_color(GREEN).next_to(arrow, UP)
+        arrow_group.add(arrow, arrow_tex)
+        result.add(arrow_group)
+
+        return result
+    
+
+class Function1(TeachFunctionScene):
 
     def setup(self):
-        func = lambda x: 2*x-1
-        function_strings = ["f", ["2", "x", "-", "1"]]
-        x_values = [-3/4, 0, 1/2, 2]
-        discontinuities = None
-        self.setup_function_stuff(func, function_strings, x_values, discontinuities)
+        x_vals = [-3/4, 0, 1/2, 2]
+        step1 = ["2", lambda x: x, "-", "1"]
+        step2 = [lambda x: 2*x, "-", "1"]
+        steps = [step1, step2]
+        self.setup_function_stuff(self.func, x_vals, steps, "x", "y", "f")
+    
+    def func(self, x):
+        return 2*x-1
+
+    def construct(self):
+
+        group = self.get_input_output_group()
+        f_tex = self.get_function_steps_group()
+        y_tex = self.get_y_tex()
+        y_tex.next_to(f_tex, UP)
+        
+        self.add(group, y_tex, f_tex)
