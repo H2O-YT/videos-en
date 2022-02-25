a39ecc4d4751e746270c9a5134262c2fa8727afc
H2O-YT <benjamin.ubilla.29@gmail.com>
2022-02-25 00:13:18 -0300

    Updated

--- 2022/precalculus/functions1.py
+++ 2022/precalculus/functions1.py
@@ -36,6 +36,12 @@ x_str = "x"
 y_str = "y"
 f_str = "f"
 func_str = ["2", lambda x: x if isinstance(x, str) else "("+str(x)+")", "-", "1"]
+x_vals = [-0.75, 0, 0.5, 1]
+steps = [[lambda x: check_and_convert_to_int(2*x), "-", "1"]]
+discontinuities = None
+
+def func(x):
+    return 2*x-1
 
 
 class FunctionIntro1(FunctionIntro):
@@ -52,4 +58,111 @@ class FunctionIntro1(FunctionIntro):
         text.to_edge(DOWN)
 
         self.play(FadeIn(text, shift=UP))
-        self.wait(2)
+        self.wait()
+
+
+class FunctionMapping1(FunctionMapping):
+
+    def setup(self):
+        self.setup_function_stuff(x_vals, steps, x_str, y_str, f_str, func_str)
+
+    def func(self, x):
+        return func(x)
+
+
+class VisualizeFunction1(VisualizeFunction):
+
+    def setup(self):
+        self.setup_function_stuff(x_vals, discontinuities, x_str, y_str, f_str, func_str)
+    
+    def func(self, x):
+        return func(x)
+
+
+def get_string1(x):
+    return x
+
+def get_string2(x):
+    if x-1 >= 0:
+        return "{"+str(x-1)+"^"
+    else:
+        return "{("+str(x-1)+")^"
+
+def get_string3(x):
+    return str(x-1)+"}"
+
+def get_string4(x):
+    return "{"+str((x-1)**3)
+
+x_str2 = "x"
+y_str2 = "y"
+f_str2 = "g"
+func_str2 = ["{(", get_string1, "-", "1", ")^", "3", "\\over", get_string1, "-", "1}"]
+x_vals2 = range(-1, 3)
+step1func2 = [get_string2, "3", "\\over", get_string3]
+step2func2 = [get_string4, "\\over", get_string3]
+steps2 = [step1func2, step2func2]
+discontinuities2 = [RIGHT]
+
+def func2(x):
+    return (x-1)**3/(x-1)
+
+
+class FunctionIntro2(FunctionIntro):
+
+    def setup(self):
+        self.setup_function_stuff(x_str2, y_str2, f_str2, func_str2)
+
+
+class FunctionMapping2(FunctionMapping):
+
+    def setup(self):
+        self.setup_function_stuff(x_vals2, steps2, x_str2, y_str2, f_str2, func_str2)
+    
+    def func(self, x):
+        return func2(x)
+
+
+class VisualizeFunction2(VisualizeFunction):
+
+    def setup(self):
+        self.setup_function_stuff(x_vals2, discontinuities2, x_str2, y_str2, f_str2, func_str2)
+    
+    def func(self, x):
+        return func2(x)
+
+
+x_str3 = "t"
+y_str3 = "y"
+f_str3 = "h"
+func_str3 = ["{1", "\\over", lambda x: x+"}" if isinstance(x, str) else str(x)+"}"]
+x_vals3 = [-3, -0.5, 0, 0.5, 3]
+steps3 = None
+discontinuities3 = [np.array([0, None], dtype=float or NoneType)]
+
+def func3(x):
+    return 1/x
+
+
+class FunctionIntro3(FunctionIntro):
+
+    def setup(self):
+        self.setup_function_stuff(x_str3, y_str3, f_str3, func_str3)
+
+
+class FunctionMapping3(FunctionMapping):
+
+    def setup(self):
+        self.setup_function_stuff(x_vals3, steps3, x_str3, y_str3, f_str3, func_str3)
+    
+    def func(self, x):
+        return func3(x)
+
+
+class VisualizeFunction3(VisualizeFunction):
+
+    def setup(self):
+        self.setup_function_stuff(x_vals3, discontinuities3, x_str3, y_str3, f_str3, func_str3)
+    
+    def func(self, x):
+        return func3(x)
--- 2022/precalculus/precalculus_basics.py
+++ 2022/precalculus/precalculus_basics.py
@@ -8,7 +8,7 @@ sys.path.insert(0, path_folder)
 
 from basics import *
 from copy import deepcopy
-from types import FunctionType
+from types import FunctionType, NoneType
 
 
 class PrecalculusIntro(Scene):
@@ -61,7 +61,7 @@ class FunctionIntro(Scene):
         
         self.play(Write(text))
         self.play(GrowFromCenter(group[1:]))
-        self.wait(2)
+        self.wait()
 
 
 class FunctionMapping(Scene):
@@ -72,11 +72,10 @@ class FunctionMapping(Scene):
         for x_val in x_vals:
             try:
                 y_val = self.func(x_val)
+                self.y_vals.append(y_val)
             except:
-                pass
-            finally:
                 y_val = "Impossible"
-            self.y_vals.append(y_val)
+                self.y_vals.append(y_val)
         self.steps = steps
         self.x_str = x_str
         self.y_str = y_str
@@ -121,25 +120,49 @@ class FunctionMapping(Scene):
 
             group = VGroup()
             func_str = deepcopy(self.func_str)
+            
+            x_val_decimals = len(str(x_val).split(".")[1])
+
+            if x_val_decimals <= 3:
+                x_val_rounded = check_and_convert_to_int(np.round(x_val, decimals=x_val_decimals))
+
+            else:
+                x_val_rounded = check_and_convert_to_int(np.round(x_val, decimals=3))
+                x_val_rounded = str(x_val_rounded)+"\\ldots"
 
             for i in range(len(func_str)):
                 if isinstance(func_str[i], FunctionType):
-                    func_str[i] = func_str[i](x_val)
+                    func_str[i] = func_str[i](x_val_rounded)
 
-            tex = MathTex(self.f_str, "(", self.x_str, ")", "=", *func_str)
+            tex = MathTex(self.f_str, "(", x_val_rounded, ")", "=", *func_str)
             group.add(tex)
 
-            steps = deepcopy(self.steps)
-            
-            for step in steps:
-                for i in range(len(step)):
-                    if isinstance(step[i], FunctionType):
-                        step[i] = step[i](x_val)
-                tex = MathTex(self.f_str, "(", x_val, ")", "=", *step)
-                group.add(tex)
-            
+            if self.steps != None:
+
+                steps = deepcopy(self.steps)
+                
+                for step in steps:
+                    for i in range(len(step)):
+                        if isinstance(step[i], FunctionType):
+                            step[i] = step[i](x_val)
+                            step_part_decimals = len(str(step[i]).split(".")[1])
+                            if step_part_decimals <= 3:
+                                step[i] = check_and_convert_to_int(np.round(step[i], decimals=step_part_decimals))
+                            else:
+                                step[i] = check_and_convert_to_int(np.round(step[i], decimals=3))
+                                step[i] = str(x_val_rounded)+"\\ldots"
+                    tex = MathTex(self.f_str, "(", x_val_rounded, ")", "=", *step)
+                    group.add(tex)
+
             if y_val != "Impossible":
-                tex = MathTex(self.f_str, "(", x_val, ")", "=", check_and_convert_to_int(y_val))
+                y_val_decimals = len(str(y_val).split(".")[1])
+                if step_part_decimals <= 3:
+                    y_val_rounded = check_and_convert_to_int(np.round(y_val, decimals=y_val_decimals))
+                else:
+                    y_val_rounded = check_and_convert_to_int(np.round(y_val, decimals=3))
+                    y_val_rounded = str(y_val_rounded)+"\\ldots"
+
+                tex = MathTex(self.f_str, "(", x_val_rounded, ")", "=", y_val_rounded)
             else:
                 tex = Tex(y_val).set_color(RED)
             group.add(tex)
@@ -154,7 +177,7 @@ class FunctionMapping(Scene):
             else:
                 for tex in result[i]:
                     tex.set_color_by_tex(self.f_str, GREEN)
-        
+
         return result
     
     def get_mapping_group(self):
@@ -165,7 +188,14 @@ class FunctionMapping(Scene):
         x_vals_group = VGroup()
 
         for x_val in self.x_vals:
-            tex = MathTex(self.x_str, "=", x_val)
+            x_val_decimals = len(str(x_val).split(".")[1])
+            if x_val_decimals <= 3:
+                x_val_rounded = check_and_convert_to_int(np.round(x_val, decimals=x_val_decimals))
+            else:
+                x_val_rounded = check_and_convert_to_int(np.round(x_val, decimals=3))
+                x_val_rounded = str(x_val_rounded)+"\\ldots"
+
+            tex = MathTex(self.x_str, "=", x_val_rounded)
             x_vals_group.add(tex)
 
         x_vals_group.arrange(DOWN)
@@ -176,16 +206,22 @@ class FunctionMapping(Scene):
 
         for y_val in self.y_vals:
             if y_val != "Impossible":
-                tex = MathTex(self.y_str, "=", y_val)
+                y_val_decimals = len(str(x_val).split(".")[1])
+                if y_val_decimals <= 3:
+                    y_val_rounded = check_and_convert_to_int(np.round(y_val, decimals=y_val_decimals))
+                else:
+                    y_val_rounded = check_and_convert_to_int(np.round(y_val, decimals=3))
+                    y_val_rounded = str(y_val_rounded)+"\\ldots"
+                tex = MathTex(self.y_str, "=", y_val_rounded)
             else:
                 tex = Tex(y_val).set_color(RED)
-            y_vals_group.add(y_group)
+            y_vals_group.add(tex)
 
         y_vals_group.arrange(DOWN)
         y_group.add(y_vals_group)
 
         total_groups = VGroup(x_group, y_group)
-        group = VGroup(x_vals_group, y_vals_group).arrange(RIGHT, buff=4)
+        group = VGroup(x_vals_group, y_vals_group).arrange(RIGHT, buff=2)
         colors = [YELLOW, BLUE]
         strings = ["A", "B"]
 
@@ -197,7 +233,7 @@ class FunctionMapping(Scene):
 
             shape_group = VGroup()
             
-            ellipse = Ellipse(width = vals_group.width+1, height=vals_group.height+1)
+            ellipse = Circle(radius=vals_group.width/2)
             ellipse.set_color(color)
             ellipse.surround(vals_group)
             tex = MathTex(string).set_color(color).next_to(ellipse, UP)
@@ -209,10 +245,254 @@ class FunctionMapping(Scene):
         set1 = x_group[1][1]
         set2 = y_group[1][1]
         arrow = CurvedArrow(
-            set1.get_top()+0.1*UP, set2.get_top()+0.1*UP, angle=-90*DEGREES
+            set1.get_top()+0.1*UP, set2.get_top()+0.1*UP, angle=-45*DEGREES
         ).set_color(PURPLE)
         tex = MathTex(self.f_str).set_color(GREEN).next_to(arrow, UP)
         arrow_group.add(arrow, tex)
         result.add(x_group, y_group, arrow_group)
 
         return result
+    
+    def construct(self):
+
+        mapping_group = self.get_mapping_group()
+        f_steps_group = self.get_f_steps_group()
+        y_group = self.get_y_group()
+
+        f_steps_group.to_edge(DOWN)
+        y_group.next_to(f_steps_group, UP)
+
+        x_map_group = mapping_group[0]
+        y_map_group = mapping_group[1]
+        arrow_group = mapping_group[2]
+        
+        x_vals_group = x_map_group[0]
+        y_vals_group = y_map_group[0]
+
+        f_general = f_steps_group[0]
+        y_general = y_group[0]
+        
+        self.play(Write(x_map_group))
+        self.play(Write(y_general))
+        self.play(Write(f_general))
+
+        for i in range(len(self.x_vals)):
+
+            x_val = x_vals_group[i]
+            self.play(Indicate(x_val))
+            self.wait()
+
+            f_general.save_state()
+            y_general.save_state()
+
+            f_steps = f_steps_group[1:][i]
+            y_val1 = y_group[1:][i]
+
+            for j in range(len(f_steps)):
+                anims = [Transform(f_general, f_steps[j])]
+                if j == 0:
+                    anims.append(Transform(y_general, y_val1))
+                self.play(*anims)
+                self.wait()
+            
+            self.play(Circumscribe(f_general))
+            self.play(Indicate(y_general))
+            self.wait()
+            
+            y_val2 = y_vals_group[i]
+
+            self.play(Write(y_val2))
+            self.wait()
+
+            arrow = Arrow(x_val.get_right()+0.1*RIGHT, y_val2.get_left()+0.1*LEFT)
+            self.play(Create(arrow))
+            self.wait()
+            self.play(Uncreate(arrow))
+            self.play(Restore(f_general), Restore(y_general))
+            self.wait()
+        
+        y_group_set = y_map_group[1]
+        self.play(Write(y_group_set))
+        self.wait()
+        self.play(Write(arrow_group))
+        self.wait()
+
+
+class VisualizeFunction(Scene):
+
+    def setup_function_stuff(self, x_vals, discontinuities, x_str, y_str, f_str, func_str):
+        self.x_vals = x_vals
+        self.y_vals = []
+        for x_val in x_vals:
+            try:
+                y_val = check_and_convert_to_int(self.func(x_val))
+                self.y_vals.append(y_val)
+            except:
+                y_val = "Impossible"
+                self.y_vals.append(y_val)
+        self.discontinuities = discontinuities
+        self.x_str = x_str
+        self.y_str = y_str
+        self.f_str = f_str
+        self.func_str = func_str
+    
+    def func(self, x):
+        pass
+
+    def get_general_group(self):
+
+        result = VGroup()
+
+        y_general = MathTex(self.y_str, "=", self.f_str, "(", self.x_str, ")")
+        result.add(y_general)
+
+        func_str = deepcopy(self.func_str)
+
+        for i in range(len(func_str)):
+            if isinstance(func_str[i], FunctionType):
+                func_str[i] = func_str[i](self.x_str)
+        
+        f_general = MathTex(self.f_str, "(", self.x_str, ")", "=", *func_str)
+        result.add(f_general)
+
+        for tex in result:
+            tex.set_color_by_tex(self.x_str, YELLOW)
+            tex.set_color_by_tex(self.y_str, BLUE)
+            tex.set_color_by_tex(self.f_str, GREEN)
+        
+        result.arrange(DOWN)
+
+        return result
+    
+    def get_x_vals(self):
+
+        result = VGroup()
+
+        for x_val in self.x_vals:
+            tex = MathTex(self.x_str, "=", x_val)
+            tex.set_color_by_tex(self.x_str, YELLOW)
+            result.add(tex)
+        
+        result.arrange(DOWN)
+        
+        return result
+    
+    def get_y_vals(self):
+        
+        result = VGroup()
+
+        for y_val in self.y_vals:
+            if y_val != "Impossible":
+                tex = MathTex(self.y_str, "=", y_val)
+                tex.set_color_by_tex(self.y_str, BLUE)
+                result.add(tex)
+            else:
+                tex = Tex(y_val).set_color(RED)
+                result.add(tex)
+        
+        result.arrange(DOWN)
+
+        return result
+
+    def show_title_part(self):
+        title = TexRainbow("Let's visualize our function!").scale(2)
+        self.play(Write(title))
+        self.wait()
+        self.play(Unwrite(title))
+        self.wait()
+
+    def visualize_function(self):
+
+        ax = NumberPlane().add_coordinates()
+
+        if self.discontinuities == None:
+            graph = FunctionGraph(self.func, color=ORANGE)
+        else:
+            discontinuities = set([discontinuity[0] for discontinuity in self.discontinuities])
+            graph = FunctionGraph(self.func, color=ORANGE, discontinuities=discontinuities)
+
+        general_group = self.get_general_group()
+        x_vals = self.get_x_vals()
+        y_vals = self.get_y_vals()
+
+        group = VGroup(general_group, x_vals).arrange(DOWN).to_corner(UL)
+        rec = always_redraw(lambda: SurroundingRectangle(group).set_fill(BLACK, opacity=1))
+
+        self.play(Write(ax))
+        self.play(Write(rec), Write(group))
+        self.wait()
+        self.play(Indicate(x_vals))
+        self.wait()
+
+        dots = VGroup()
+
+        for x in self.x_vals:
+            dot = Dot(ax.c2p(x, 0)).set_color(ORANGE)
+            dots.add(dot)
+            self.play(Create(dot))
+        
+        self.wait()
+        self.play(group.animate.become(VGroup(general_group, y_vals).arrange(DOWN).to_corner(UL)))
+        self.play(Indicate(group[1]))
+        self.wait()
+
+        for i in range(len(self.y_vals)):
+            if self.y_vals[i] != "Impossible":
+                self.bring_to_back(ax, dots[i])
+                self.play(dots[i].animate.shift(self.y_vals[i]*UP))
+            else:
+                self.play(Uncreate(dots[i]))
+        
+        self.wait()
+
+        text = Tex("Let's extend ", "$A$ ", "to more values!").to_edge(DOWN)
+        text.set_color_by_tex("$A$", YELLOW)
+        rec2 = SurroundingRectangle(text).set_fill(BLACK, opacity=1)
+        self.play(Write(rec2), Write(text))
+        self.wait(2)
+        self.play(Unwrite(rec2), ShrinkToCenter(text))
+        self.wait()
+        self.play(FadeOut(dots))
+        self.bring_to_back(ax, graph)
+        self.play(Create(graph))
+
+        if self.discontinuities != None:
+
+            for i in range(len(self.discontinuities)):
+                discontinuity = self.discontinuities[i]
+                nondefined_points = VGroup()
+                defined_points = VGroup()
+                try:
+                    y_val = self.func(discontinuity[0])
+                    if discontinuity[1] != None:
+                        if discontinuity[1] != y_val:
+                            dot = Dot(discontinuity).set_fill(BLACK, opacity=1).set_stroke(ORANGE, width=4)
+                            nondefined_points.add(dot)
+                        else:
+                            dot = Dot(discontinuity).set_fill(ORANGE, opacity=1).set_stroke(ORANGE, width=4)
+                            defined_points.add(dot)
+                        self.play(Create(dot))
+                except:
+                    if discontinuities[1] != None:
+                        dot = Dot(discontinuity).set_fill(BLACK, opacity=1).set_stroke(ORANGE, width=4)
+                        nondefined_points.add(dot)
+                        self.play(Create(dot))
+
+            self.wait()
+            dot = nondefined_points[0].copy()
+            tex = Tex("means the graph doesn't include that point")
+            aclaration = VGroup(dot, tex).arrange(RIGHT).to_edge(DOWN)
+            rec = always_redraw(lambda: SurroundingRectangle(aclaration).set_fill(BLACK, opacity=1))
+            self.play(Write(rec), Write(aclaration))
+
+            if len(defined_points) > 0:
+                self.wait(2)
+                dot = defined_points[0].copy()
+                tex2 = Tex("means the graph includes that point")
+                self.play(aclaration.animate.become(VGroup(dot, tex2).arrange(RIGHT).to_edge(DOWN)))
+
+        self.wait(2)
+
+    def construct(self):
+        self.show_title_part()
+        self.visualize_function()
